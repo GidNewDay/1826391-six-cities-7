291a3992073a0ceee753163b8cc46293806fcc0d
Навруз <newday.88@mail.ru>
2021-06-21 13:26:58 +0300

    Добавил моки (мнимые данные) для предложений (оферов)

---
+++ project/src/mocks/offers.js
@@ -0,0 +1,60 @@
+const offers = [
+  {
+    id: 1,
+    name: 'Beautiful & luxurious apartment at great location',
+    type: 'Apartment',
+    price: 120,
+    star: 4,
+    premium: true,
+    images: [{
+      src1: 'img/apartment-01.jpg',
+      src2: 'img/apartment-02.jpg',
+      src3: 'img/apartment-03.jpg',
+      src4: 'img/apartment-04.jpg',
+    }],
+  },
+  {
+    id: 2,
+    name: 'Wood and stone place',
+    type: 'Private room',
+    price: 80,
+    star: 4,
+    premium: false,
+    images: [{
+      src1: 'img/apartment-01.jpg',
+      src2: 'img/apartment-02.jpg',
+      src3: 'img/apartment-03.jpg',
+      src4: 'img/apartment-04.jpg',
+    }],
+  },
+  {
+    id: 3,
+    name: 'Canal View Prinsengracht',
+    type: 'Apartment',
+    price: 132,
+    star: 4,
+    premium: false,
+    images: [{
+      src1: 'img/apartment-01.jpg',
+      src2: 'img/apartment-02.jpg',
+      src3: 'img/apartment-03.jpg',
+      src4: 'img/apartment-04.jpg',
+    }],
+  },
+  {
+    id: 4,
+    name: 'Nice, cozy, warm big bed apartment',
+    type: 'Apartment',
+    price: 180,
+    star: 4,
+    premium: true,
+    images: [{
+      src1: 'img/apartment-01.jpg',
+      src2: 'img/apartment-02.jpg',
+      src3: 'img/apartment-03.jpg',
+      src4: 'img/apartment-04.jpg',
+    }],
+  },
+];
+
+export default offers;
