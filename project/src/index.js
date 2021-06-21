6e18c33106319e837b7025f0236075c134367d08
Навруз <newday.88@mail.ru>
2021-06-21 13:37:04 +0300

    Подключил моки в App

--- project/src/index.js
+++ project/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import offers from "./mocks/offers";
 
 const CARDS_AMOUNT = 5;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App cardsAmount={CARDS_AMOUNT}/>
+    <App
+      cardsAmount={CARDS_AMOUNT}
+      offers={offers}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
